52c5e624443297fdc3e9eb5fae570a1f37273193
lvs <lvs.freelance@gmail.com>
2016-10-03 15:17:06 +0700

    Last commit

---
+++ app/src/main/java/com/lvsandroid/udemynewsreader/ArticleActivity.java
@@ -0,0 +1,51 @@
+package com.lvsandroid.udemynewsreader;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class ArticleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_article);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        WebView webView = (WebView)findViewById(R.id.webView);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+
+        Intent i = getIntent();
+
+        String url = i.getStringExtra("articleUrl");
+        //String content = i.getStringExtra("content");
+
+        webView.loadUrl(url);
+        //webView.loadData(content, "text/html", "UTF-8");
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+}
